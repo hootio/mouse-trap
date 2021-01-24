9a54fedbc85917c4b289751a4b94f80ae591a737
Hootan Shadmehr <hootan@umich.edu>
2021-01-23 20:08:43 -0500

    Init

---
+++ src/extension.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+
+let on = true;
+let count = 1;
+
+function activate(context: vscode.ExtensionContext) {
+  console.log('mouse-trap activated!');
+
+  let disposable = vscode.commands.registerCommand('mouse-trap.toggle', () => {
+    on = !on;
+    vscode.window.showInformationMessage(`Toggled ${on ? 'On' : 'Off'}`);
+  });
+
+  vscode.window.onDidChangeTextEditorSelection((event) => {
+    if (on) {
+      mouseTrackerCallback(event);
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {
+  console.log('mouse-trap deactivated!');
+}
+
+function mouseTrackerCallback(event: vscode.TextEditorSelectionChangeEvent) {
+  if (event.kind === vscode.TextEditorSelectionChangeKind.Mouse) {
+    vscode.window.showInformationMessage(
+      `Really, mouse again? (failure count: ${count})`
+    );
+    count++;
+  }
+}
+
+export { activate, deactivate };
+
+/**
+ * TODOs:
+ * - add more sassssss
+ * - add cmd + K, cmd + S to pop up message
+ * - have "cmd" in pop-up message be OS/customization specific
+ */
